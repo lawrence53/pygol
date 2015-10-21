26030a6dfa922780aec064b34a7455efe0cef6e9
LMitchell <lawrenceomitchell@gmail.com>
2015-10-21 08:39:04 +0100

    Got an array init working.

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,6 +1,21 @@
+import numpy as np
+
 class GameOfLife():
+  array   = []
+  rows    = []
+  cols    = []
+  
   def __init__(self):
     print("Initializing game!")
 	
+  def InitArrayZeros(self, rows, cols):
+    self.rows = rows
+    self.cols = cols
+    self.array = np.zeros((rows,cols), dtype=np.bool_)
+	
 if __name__ == "__main__":
   g = GameOfLife()
+  print (g.array)
+  g.InitArrayZeros(4,5)
+  print (g.array)
+  
