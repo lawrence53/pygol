44919f9c59da938ea188d9ad0a9b93e3da6a3fa7
LMitchell <lawrenceomitchell@gmail.com>
2015-10-22 09:28:50 +0100

    Added checker board init.

--- GameOfLife.py
+++ GameOfLife.py
@@ -2,8 +2,8 @@ import numpy as np
 
 class GameOfLife():
   array   = []
-  rows    = []
-  cols    = []
+  rows    = 0
+  cols    = 0
   
   def __init__(self):
     print("Initializing game!")
@@ -12,10 +12,18 @@ class GameOfLife():
     self.rows = rows
     self.cols = cols
     self.array = np.zeros((rows,cols), dtype=np.bool_)
+
+  def InitArrayChecker(self):
+    for r in range (0, self.rows):
+      for c in range (0, self.cols):
+        if (r & 0x1) ^ (c & 0x1):
+          self.array[r,c] = True
 	
 if __name__ == "__main__":
   g = GameOfLife()
   print (g.array)
   g.InitArrayZeros(4,5)
+  print (g.array)
+  g.InitArrayChecker()
   print (g.array)
   
