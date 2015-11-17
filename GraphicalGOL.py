c3b026536f19d52f2de7b3d69f17a46becf5ee57
LMitchell <lawrenceomitchell@gmail.com>
2015-11-17 22:40:09 +0000

    Working quite nice with matplotlib

--- GraphicalGOL.py
+++ GraphicalGOL.py
@@ -1,11 +1,25 @@
-from scipy.misc.pilutil import toimage
+import matplotlib.pyplot as plt
 import GameOfLife as GOL
+import matplotlib.cm as cm
+import time
 
 if __name__ == "__main__":
   g = GOL.GameOfLife()
-  g.InitArrayZeros(200,300)
+  g.InitArrayZeros(100,150)
   g.InitArrayRandom()
-  for i in range (0, 5):
-    toimage(g.array).show()
+  
+  fig = plt.figure()
+  fig.show()
+  #fig.add_axes(aspect="equal",extent=[0, g.rows, 0, g.cols])
+  #fig_ax = fig.gca() #Get the current axis
+  fig_ax = fig.add_subplot(111)
+
+  for i in range (0, 1000):
     g.LifeStep()
-    toimage(g.array).show()
+    #g.PrettyPrint()
+    fig_ax.imshow(g.array, cm.gray)
+    fig.canvas.draw()
+    print("Life step:{0:d}\n", i)
+    time.sleep(0.1)
+    fig_ax.clear()
+    
